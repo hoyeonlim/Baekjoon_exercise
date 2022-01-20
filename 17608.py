67e5f39a8824bff55f376455ec0e427c9b35153d
hoyeonlim <limhoyeon@hanmail.net>
2022-01-20 23:11:27 +0900

    2022.01.20

---
+++ 17608.py
@@ -0,0 +1,15 @@
+import sys
+n = int(sys.stdin.readline())
+arr = [0 for i in range(n)]
+
+for j in range(n):
+    arr[j] = int(sys.stdin.readline())
+max = arr[n-1]
+cnt = 1
+for k in range(n-2, -1, -1):
+    if arr[k] > max:
+        max = arr[k]
+        cnt += 1
+    else:
+        continue
+print(cnt)
--- 2579.py
+++ 2579.py
@@ -13,5 +13,5 @@ ans[2] = max(arr[1]+arr[2], arr[0]+arr[2])
 
 for j in range (3, n):
     ans[j] = max(ans[j-3]+arr[j-1]+arr[j], ans[j-2]+arr[j])
-print(ans)
+
 print(ans[n-1])
